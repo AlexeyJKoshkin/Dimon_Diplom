f7730e93b2c072f3732a97ce0b77893f1103b44e
James Sanderland <AlexeyJKoshkin@gmail.com>
2017-05-08 15:40:43 +0300

    Заготовки главного меню

---
+++ Assets/Scripts/UI/ContainerControllers/MenuItemController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using ShutEye.UI.Core;
+
+public class MenuItemController : SEContainerWithViews<MainMenuItemContainer>
+{
+    public override void RefreshView()
+    {
+    }
+}
---
+++ Assets/Scripts/UI/MainMenuWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using GameKit.UI;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MainMenuWindow : BaseWindow
+{
+    public override Enum TypeWindow
+    {
+        get { return WindowType.Menu; }
+    }
+
+    public override WindowState State { get; set; }
+
+    [SerializeField]
+    private MenuItemController _itemsContainer;
+
+
+    protected override void PrepareUI(Action _onComplete)
+    {
+        _itemsContainer.OnChange  += ItemsContainerOnOnChange;
+        base.PrepareUI(_onComplete);
+    }
+
+    private void ItemsContainerOnOnChange(MainMenuItemContainer mainMenuItemContainer, PointerEventData.InputButton inputButton)
+    {
+        this.HideWindow(null,true);
+        Debug.LogFormat("{0} Show", mainMenuItemContainer.CurrentData);
+        switch (mainMenuItemContainer.CurrentData)
+        {
+            case MenuItemType.Leading:
+            case MenuItemType.Music:
+            case MenuItemType.Fotographer:
+            case MenuItemType.Decorator:
+            case MenuItemType.Videograph:
+            case MenuItemType.ShowProgramm:
+                break;
+        }
+    }
+
+    public override void HideWindow(Action callback, bool immidiate = false)
+    {
+    }
+
+    public override void ShowWindow(Action callback, bool immidiate = false)
+    {
+    }
+
+    public override void RefreshView()
+    {
+
+    }
+}
