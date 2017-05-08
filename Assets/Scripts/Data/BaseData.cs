b86faf1f2fe5db3fdc06e695a3ef8f3a8e31341b
James Sanderland <AlexeyJKoshkin@gmail.com>
2017-05-08 15:58:41 +0300

    tryam

---
+++ Assets/Scripts/Data/BaseData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// базовые данные под отображение
+/// </summary>
+public class BaseDataForSelectWindow : DataObject
+{
+    public Sprite AvatarSprite;
+    public string Info;
+}
