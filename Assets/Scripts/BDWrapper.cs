0fe0bdf1565cdf1f4962084575e87703f48b7ccc
James Sanderland <AlexeyJKoshkin@gmail.com>
2017-05-13 01:18:43 +0300

    таблицы

--- Assets/Scripts/BDWrapper.cs
+++ Assets/Scripts/BDWrapper.cs
@@ -15,21 +15,21 @@ public static class BDWrapper
                 {
                    Id= 1,
                     Name = "Дмитрий Кошкин",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_leading\Koshkin"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_leading\Koshkin"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 2,
                     Name = "Владимир Пахомов",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_leading\Pakhomov"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_leading\Pakhomov"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 3,
                     Name = "Максим Лобов",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_leading\Lobov"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_leading\Lobov"),
                     Info = "Blabala"
                 });
                 break;
@@ -38,21 +38,21 @@ public static class BDWrapper
                 {
                     Id= 1,
                     Name = "French",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_music\French"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_music\French"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 2,
                     Name = "Dj_Lime",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_music\Dj_Lime"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_music\Dj_Lime"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 3,
                     Name = "Hobots",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_music\Hobots"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_music\Hobots"),
                     Info = "Blabala"
                 });
                 break;
@@ -61,21 +61,21 @@ public static class BDWrapper
                 {
                     Id= 1,
                     Name = "Вероника Чернявская",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Fotographer\Chernyavskaya"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Fotographer\Chernyavskaya"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 2,
                     Name = "Христя Мармонов",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Fotographer\Marmarov"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Fotographer\Marmarov"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 3,
                     Name = "Александр Сеоев",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Fotographer\Seoev"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Fotographer\Seoev"),
                     Info = "Blabala"
                 });
                 break;
@@ -84,21 +84,21 @@ public static class BDWrapper
                 {
                     Id= 1,
                     Name = "Сад",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Decorator\Sad"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Decorator\Sad"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 2,
                     Name = "Пронина Анастасия ",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Decorator\Pronina"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Decorator\Pronina"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id= 3,
                     Name = "Чудное Мгновение",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Decorator\Mgnovenie"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Decorator\Mgnovenie"),
                     Info = "Blabala"
                 });
                 break;
@@ -107,21 +107,21 @@ public static class BDWrapper
                 {
                     Id = 1,
                     Name = "Алексей Иванов",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Videograph\Ivanov"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Videograph\Ivanov"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id = 2,
                     Name = "Артем Дрягин",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Videograph\Draygin"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Videograph\Draygin"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id = 3,
                     Name = "Сергей Студеникин",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_Videograph\Studenikin"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_Videograph\Studenikin"),
                     Info = "Blabala"
                 });
                 break;
@@ -130,21 +130,21 @@ public static class BDWrapper
                 {
                     Id = 1,
                     Name = "Старт Наука",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_ShowProgramm\Start"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_ShowProgramm\Start"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id = 2,
                     Name = "Арт-Бар",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_ShowProgramm\ArtBar"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_ShowProgramm\ArtBar"),
                     Info = "Blabala"
                 });
                 result.Add(new BaseDataForSelectWindow()
                 {
                     Id = 3,
                     Name = "Бар-Молекула",
-                    AvatarSprite = GameCore.LoadSprite("Iconc\avatar_ShowProgramm\Molecula"),
+                    AvatarSprite = GameCore.LoadSprite(@"Iconc\avatar_ShowProgramm\Molecula"),
                     Info = "Blabala"
                 });
                 break;
